7a6d63c4b34ddf93f6b8c59afe885038777bcf82
Killian Brackey <killianbrackey@gmail.com>
2016-04-08 01:12:06 -0500

    Added new checksum

--- docker-clean.rb
+++ docker-clean.rb
@@ -5,7 +5,7 @@ class DockerClean < Formula
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v1.1.2.tar.gz"
   version "1.1.2"
-  sha256 ""
+  sha256 "e8b9bc9d88e96c2bfe68416855441d44d5685b9a68809d49e8a0ffff10b9ac9c"
 
   def install
     bin.install "docker-clean"
