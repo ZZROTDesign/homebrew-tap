4e6511ce5fc88f0ba42cba7350a7eebb8d972f9a
Killian Brackey <killianbrackey@gmail.com>
2016-04-07 23:27:49 -0500

    Added docker-clean rb

---
+++ docker-clean.rb
@@ -0,0 +1,20 @@
+# Docker-Clean Script
+
+class DockerClean < Formula
+  desc "A script that cleans docker containers, images, and volumes."
+  homepage "https://github.com/ZZROTDesign/docker-clean"
+  url "https://github.com/ZZROTDesign/docker-clean/archive/v1.0.3.tar.gz"
+  version "1.0.3"
+  sha256 "03fc8abb42e43f5005c2cea39a10ac4fda5b0c6aeb3a471cf56f8164ba42bc5e"
+
+
+  def install
+    bin.install "docker-clean"
+    
+  end
+
+  test do
+    system "false"
+    
+  end
+end
