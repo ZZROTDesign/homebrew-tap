1ce082195e3ecea44205b922b432ac75f1692452
Killian Brackey <killianbrackey@gmail.com>
2016-04-09 14:56:17 -0500

    Hotfix on debug statment

--- docker-clean.rb
+++ docker-clean.rb
@@ -6,17 +6,17 @@
 class DockerClean < Formula
   desc "A script that cleans docker containers, images, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
-  url "https://github.com/ZZROTDesign/docker-clean/archive/v1.1.3.tar.gz"
-  version "1.1.3"
-  sha256 "610e5f8ede201e8a709ce2e80c0b2cb3637132ae65709dca9d768527fb76068a"
+  url "https://github.com/ZZROTDesign/docker-clean/archive/v1.1.4.tar.gz"
+  version "1.1.4"
+  sha256 ""
 
   def install
     bin.install "docker-clean"
-    
+
   end
 
   test do
     system "false"
-    
+
   end
 end
