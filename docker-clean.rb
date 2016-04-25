5e516737c1199c5b495bbc56ddbb785ce47d6ae3
Killian Brackey <killianbrackey@gmail.com>
2016-04-25 02:20:14 -0500

    Added 1.3.1 sha256

--- docker-clean.rb
+++ docker-clean.rb
@@ -1,14 +1,14 @@
 # Docker-Clean Script
 # Maintained by Killian Brackey && Sean Kilgarriff at ZZROT Design
 # Contact us at docker@zzrot.com for any issues or fork and pull from
-# the github repo below.  Happy cleaning!
+# the Github repo below.  Happy cleaning!
 
 class DockerClean < Formula
   desc "A script that cleans docker containers, images, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v1.3.1.tar.gz"
   version "1.3.1"
-  sha256 ""
+  sha256 "6103cbeccf49df3edb763c9750641c0b0e452643de79035077bda1bdaa3edc1b"
 
   def install
     bin.install "docker-clean"
