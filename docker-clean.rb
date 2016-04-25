0d058e540831694f0fb766bc2d28a57dadde3d04
Killian Brackey <killianbrackey@gmail.com>
2016-04-25 11:10:23 -0500

    Fixed homebrew audit issues

--- docker-clean.rb
+++ docker-clean.rb
@@ -4,18 +4,16 @@
 # the Github repo below.  Happy cleaning!
 
 class DockerClean < Formula
-  desc "A script that cleans docker containers, images, and volumes."
+  desc "Script that cleans docker containers, images, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v1.3.1.tar.gz"
   sha256 "6103cbeccf49df3edb763c9750641c0b0e452643de79035077bda1bdaa3edc1b"
 
   def install
     bin.install "docker-clean"
-
   end
 
   test do
     system "false"
-
   end
 end
