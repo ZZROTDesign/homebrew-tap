3e1094d7c232c328034e6ee066fc11b125f0b2b7
Killian Brackey <killianbrackey@gmail.com>
2016-05-06 02:06:51 -0500

    v2.0.0

--- docker-clean.rb
+++ docker-clean.rb
@@ -6,8 +6,8 @@
 class DockerClean < Formula
   desc "Script that cleans docker containers, images, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
-  url "https://github.com/ZZROTDesign/docker-clean/archive/v1.2.0.tar.gz"
-  sha256 ""
+  url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.0.tar.gz"
+  sha256 "be81bc9dc68161cb3858aece0b9497f7beff67589408d82497fca9275fc93deb"
 
   def install
     bin.install "docker-clean"
