c4abf2cf1180c6535d7106be95014d02ab5e5cfd
Killian Brackey <killianbrackey@gmail.com>
2016-05-07 13:38:46 -0500

    shortened description for audit

--- docker-clean.rb
+++ docker-clean.rb
@@ -4,7 +4,7 @@
 # the Github repo below.  Happy cleaning!
 
 class DockerClean < Formula
-  desc "Script that cleans docker containers, images, networks, and volumes."
+  desc "Script cleans Docker containers, images, networks, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.1.tar.gz"
   sha256 "35aca23eb75043924b14b058acdc15714e19b6395ae2a32ce4ec90b88f70b6d0"
