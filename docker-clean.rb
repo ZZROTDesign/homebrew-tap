8d86579a9155a531e09f9f090dd1a9c84a41232d
Killian Brackey <killianbrackey@gmail.com>
2016-05-10 20:29:08 -0500

    updated to homebrew-core file

--- docker-clean.rb
+++ docker-clean.rb
@@ -1,14 +1,11 @@
-# Docker-Clean Script
-# Maintained by Killian Brackey && Sean Kilgarriff at ZZROT Design
-# Contact us at docker@zzrot.com for any issues or fork and pull from
-# the Github repo below.  Happy cleaning!
-
 class DockerClean < Formula
-  desc "Script cleans Docker containers, images, networks, and volumes."
+  desc "Clean Docker containers, images, networks, and volumes"
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.3.tar.gz"
   sha256 "a2e1923fe2862ca2487e39000c68cba900711b768a8920dd1b1275a2fa0e6789"
 
+  bottle :unneeded
+  
   def install
     bin.install "docker-clean"
   end
