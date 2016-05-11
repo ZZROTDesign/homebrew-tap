b74b2fc60164dd8f345a686587070d106f7ed1ea
Killian Brackey <killianbrackey@gmail.com>
2016-05-10 19:26:13 -0500

    updated sha256sum

--- docker-clean.rb
+++ docker-clean.rb
@@ -7,7 +7,7 @@ class DockerClean < Formula
   desc "Script cleans Docker containers, images, networks, and volumes."
   homepage "https://github.com/ZZROTDesign/docker-clean"
   url "https://github.com/ZZROTDesign/docker-clean/archive/v2.0.3.tar.gz"
-  sha256 "5a0fdaa872010bed1e637334cf865a4bf6a390d079c5a00d16c5a58cf8b19b7f"
+  sha256 "a2e1923fe2862ca2487e39000c68cba900711b768a8920dd1b1275a2fa0e6789"
 
   def install
     bin.install "docker-clean"
